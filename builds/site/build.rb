c90f01e3e0fff6693f73d15979fdaaa7284a6d8b
hems <hems.inlet@gmail.com>
2013-06-09 10:56:06 +0100

    adds nano compile to server build

--- builds/site/build.rb
+++ builds/site/build.rb
@@ -6,5 +6,5 @@ require 'net/ssh'
 # setup your keys, or hardcode a password?? be or not to be?
 
 s = Net::SSH.start( "server", "user", :password => "password" ) do |ssh|
-  ssh.exec "cd /Groups/Jamoma/Sites/JamomaWebSite && git pull"
+  ssh.exec "cd /Groups/Jamoma/Sites/JamomaWebSite && git pull && nanoc compile"
 end
